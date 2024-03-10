24866c6cc3d43bc624bd3c8d15586e304d30a3fa
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-10 15:14:02 +0100

    install maps

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Page from './Page';
+import Page from './components/Page';
 import Content from './components/Content';
 
 export default function App() {
